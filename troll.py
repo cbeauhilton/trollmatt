1b40bae7f4243621b09fadd1185b278ecea56b17
cbeauhilton <34774299+cbeauhilton@users.noreply.github.com>
2022-12-27 14:52:55 -0600

    Create troll.py

---
+++ troll.py
@@ -0,0 +1,24 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+_ = load_dotenv()
+
+
+def send_email(to, subject, message):
+
+    email_address = os.environ.get("EMAIL_ADDRESS")
+    email_password = os.environ.get("EMAIL_PASSWORD")
+
+    # create email
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = email_address
+    msg['To'] = to
+    msg.set_content(message)
+
+    # send email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
